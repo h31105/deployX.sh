a8f4abe4b5ed5217475e895ae2410cb39f856488
h31105 <mailintyle@gmail.com>
2020-12-06 17:32:32 +0800

    create deployX.sh

---
+++ deployX.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#MIT License
+#Copyright (c) 2020 h31105
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+#====================================================
+# System Request:Debian 9+/Ubuntu 18.04+/Centos 7+
+# Author: Miroku/h31105
+# Dscription: deployX Script
+# Official document:
+# https://www.v2ray.com/
+# https://www.v2fly.org/
+# https://github.com/XTLS
+# https://github.com/p4gefau1t/trojan-go
+# https://github.com/liberal-boy/tls-shunt-proxy
+# https://www.docker.com/
+# https://github.com/containrrr/watchtower
+# https://github.com/portainer/portainer
+# https://github.com/wulabing/V2Ray_ws-tls_bash_onekey
+#====================================================
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+cd "$(
+    cd "$(dirname "$0")" || exit
+    pwd
+)" || exit
